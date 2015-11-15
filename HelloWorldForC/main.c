dbc4adfce8eacc38a1e5fff880536605ec4ae92c
lynn.zhu <zxl20zxl@gmail.com>
2015-11-15 19:18:45 +0800

    first commit

---
+++ HelloWorldForC/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  HelloWorldForC
+//
+//  Created by 张晓岚 on 15/11/15.
+//  Copyright (c) 2015年 ThinkMobile. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    
+    int *a = 0;
+    
+    printf("a:%d", a);
+    
+    return 0;
+}
